4daefd5b47331ced364ad48d88d3a3750d1e938f
Ranwala, Ravindra <Ravindra.Ranwala@pearson.com>
2017-04-05 09:07:45 +0530

    typo error was fixed.

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,7 +15,7 @@ export function fetchPosts(){
   };
 }
 
-export function createpost(props) {
+export function createPost(props) {
   console.log(props);
   const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
   return {
